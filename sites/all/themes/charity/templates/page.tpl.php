b416061c4abbb4ae7f3bf92bce7fb2947ac65d9d
Humphrey <humphrey.asante@gmail.com>
2018-02-11 21:39:44 +0300

    Added empty page template file

---
+++ sites/all/themes/charity/templates/page.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * Regions:
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlighted']: Items for the highlighted content region.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
+ * - $page['footer']: Items for the footer region.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see template_process()
+ * @see html.tpl.php
+ *
+ * @ingroup themeable
+ */
+?>
+<div>
+  
+</div> <!-- /.wrapper -->
