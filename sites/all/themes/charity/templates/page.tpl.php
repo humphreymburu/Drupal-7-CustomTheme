abe984f62d5ca818d38b2f553a9a426d643c9754
Humphrey <humphrey.asante@gmail.com>
2018-02-12 21:59:47 +0300

    Added theme page layout structure

--- sites/all/themes/charity/templates/page.tpl.php
+++ sites/all/themes/charity/templates/page.tpl.php
@@ -18,6 +18,112 @@
  * @ingroup themeable
  */
 ?>
-<div>
+  <header class="l-header">
+      <div class="col-1">
+      <div class="logo">open charity</div>
+      </div>
+  </header><!-- .header -->
   
-</div> <!-- /.wrapper -->
+  <main class="l-main">
+    
+  <section class="region-content region-content__color grid">
+      <div class="col-1">
+          <h2>New Events </h2>
+              <p>cancer research uk</p>
+        </div>
+    </section><!-- .events -->
+  
+
+    <section class="col-2 grid">
+
+    <div class="col-2__title"><h2 class="content-title">Mission</h2></div>
+    
+        <ul>
+  
+          <li>
+          <p>test</p>
+          </li>
+          <li>
+          <p>test2</p>
+          </li>
+          <li>
+           <p>test3</p>
+          </li>
+        </ul>
+      </section>
+
+
+      <section class="col-2 grid">
+      <div class="col-2__title"><h2 class="content-title">Mission</h2></div>
+    
+        <ul>
+  
+          <li>
+          <p>test</p>
+          </li>
+          <li>
+          <p>test2</p>
+          </li>
+          <li>
+           <p>test3</p>
+          </li>
+        </ul>
+      </section>
+        </section>
+
+
+
+
+
+
+
+
+
+        <section class="col-1 grid">
+            <ul>
+                <li>
+                   <p>test3</p>
+                  </li>
+      
+              <li>
+               <p>test3</p>
+              </li>
+              <li>
+                <p>test3</p>
+              </li>
+              <li>
+               <p>test3</p>
+              </li>
+            </ul>
+          </section>
+
+
+          <section class="col-1 grid">
+          <ul>
+                <li>
+                   <p>test3</p>
+                  </li>
+      
+              <li>
+               <p>test3</p>
+              </li>
+              <li>
+                <p>test3</p>
+              </li>
+              <li>
+               <p>test3</p>
+              </li>
+            </ul>
+            </section>
+  
+
+  </main>
+  
+  <footer class="colophon grid">
+
+    <div class="more-content">
+        <h2 class="content-title">Footer</h2>
+        <p>Test<p>
+      </div><!-- .more-content -->
+
+  </footer>
