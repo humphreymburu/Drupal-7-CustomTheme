f4f697cebed4be55dc7bfe2ecf4b90be1bf1bbb4
Humphrey <humphrey.asante@gmail.com>
2018-02-15 15:37:23 +0300

    Added theme region grid styles

--- sites/all/themes/charity/templates/page.tpl.php
+++ sites/all/themes/charity/templates/page.tpl.php
@@ -18,9 +18,9 @@
  * @ingroup themeable
  */
 ?>
-  <div class="wrapper">
+
   <header class="l-header">
-    <div class="l-header__inner">
+   <div class="l-header__inner">
       <div class="branding">
              <?php if (theme_get_setting('branding_type') == 'logo'): ?>
                 <div class="branding__logo">
@@ -50,40 +50,13 @@
               ),
             )); ?>
           </nav> <!-- navigation -->
-        </div>  
+        
         <?php endif; ?>
     </div>
+          </div>
   </header><!-- .header -->
   
-  <section id="core-outer-wrapper" class="l-outer">
-  
-    <div class="l-outer__inner">
-    
-     <div id="highlighted" class="highlighted">
-      <?php print $messages; ?>
-      
-      <?php if ($breadcrumb): ?>
-      <div id="breadcrumb"><?php print $breadcrumb; ?>
-      </div>
-      <?php endif; ?>
-      
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-    
-      <?php if ($page['highlighted']): ?>
-        <?php print render($page['highlighted']); ?>
-      <?php endif; ?>
-    </div>
-  </section>
-
-    <?php
-    // need some good cleanup on these default Drupal elements
-    ?>
+ 
  
  <?php
       // Render the sidebars to see if there's anything in them.
@@ -107,6 +80,33 @@
 
 
  <main id="content" class="<?php print $content_class; ?>" role="main">
+ <section id="core-outer-wrapper" class="l-outer">
+  
+    <div class="l-outer__inner">
+    
+     <div id="highlighted" class="highlighted">
+      <?php print $messages; ?>
+      
+      <?php if ($breadcrumb): ?>
+      <div id="breadcrumb"><?php print $breadcrumb; ?>
+      </div>
+      <?php endif; ?>
+      
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+      <?php print render($title_suffix); ?>
+      
+      <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+    
+      <?php if ($page['highlighted']): ?>
+        <?php print render($page['highlighted']); ?>
+      <?php endif; ?>
+    </div>
+  </section>
+  <br/>
         <?php print render($page['content']); ?>
         <?php print $feed_icons; ?>
       </main> <!-- /.section, /#content -->
@@ -135,4 +135,3 @@
   <?php endif; ?>
 
 
-          </div>
